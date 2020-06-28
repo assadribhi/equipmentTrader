5f836d67394fc32b2919d672594b4f0da2538890
assadribhi <assadribhi@gmail.com>
2020-06-28 20:01:03 +0300

    My Header

--- src/App.js
+++ src/App.js
@@ -1,24 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+const style = {
+  text: { textAlign: "center" },
+  yardImage: {
+    display: "block",
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: "20%",
+  },
+  equipmentImage: { width: "300px" },
+  list: {
+    alignItems: "center",
+    justifyContent: "center",
+    display: "flex",
+  },
+  equipment: { margin: "20px" },
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <h1 style={style.text}>Heavy Equipment Trader</h1>
+
+        <img
+          src="https://previews.123rf.com/images/kovacevic/kovacevic1302/kovacevic130200009/17794615-under-construction-sign-with-helmet-and-heavy-machine.jpg"
+          alt="Shop Logo"
+          style={style.yardImage}
+        />
+        <h4 style={style.text}>
+          Your Destination for all your Heavy Equipment needs.
+        </h4>
+      </div>
+      <div style={style.list}>
+        <div style={style.equipment}>
+          <img
+            src="https://www.constructionequipment.com/sites/ce/files/styles/large/public/Volvo-L180H-Bucketful.gif?itok=Rcxl2_Zp"
+            alt="Wheel loader"
+            style={style.equipmentImage}
+          />
+          <p style={style.text}>Wheel Loader Rental</p>
+          <p style={style.text}>70 KD / Day</p>
+        </div>
+        <div style={style.equipment}>
+          <img
+            src="https://www.constructionequipment.com/sites/ce/files/Caterpillar-140-motor-grader.png"
+            alt="Grader"
+            style={style.equipmentImage}
+          />
+          <p style={style.text}>Grader Rental</p>
+          <p style={style.text}>100 KD / Day</p>
+        </div>
+        <div style={style.equipment}>
+          <img
+            src="https://media.stokker.com/prod/l/982/144736982"
+            alt="Skid Steer"
+            style={style.equipmentImage}
+          />
+          <p style={style.text}>Skid Steer Rental</p>
+          <p style={style.text}>40 KD / Day</p>
+        </div>
+      </div>
     </div>
   );
 }
